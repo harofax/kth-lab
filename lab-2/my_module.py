57d5e9bcc29d6c2b508c2727c5cdcef6626ddfa1
Zhulhian <Penguin.,42>
2019-01-19 19:22:03 +0100

    done with lab 2

--- lab-2/my_module.py
+++ lab-2/my_module.py
@@ -3,9 +3,10 @@ import copy
 import math
 import random
 
+random.seed()
+
 
 # Uppgift 1 (givet)
-
 def scope_testing_function(x, x_list):
     print("Inside function: x=" + str(x) + " and x_list=" + str(x_list) + " and y=" + str(y))
     x = 1
@@ -23,13 +24,68 @@ print("Outside function: x=" + str(x) + " and x_list=" + str(x_list) + " and y="
 scope_testing_function(x, x_list)
 print("Outside function: x=" + str(x) + " and x_list=" + str(x_list) + " and y=" + str(y))
 
+
 # Uppgift 2 (att skrivas)
+def my_function(x):
+    return math.sin(x)**2 + x**2
 
 
 # Uppgift 3 (att skrivas)
-
+def roll_dice(n):
+    dice_sum = 0
+    for i in range(n):
+        dice_sum += random.randint(1, 6)
+    return dice_sum
 
 # Uppgift 4 (att skrivas)
+def my_sort_list(list):
+    list_length = len(list)
+    sorted_list = list
+
+    # Go through the list
+    for i in range(list_length):
+
+        # go to elements that are already sorted,
+        # so it doesn't go through already sorted elements
+        for j in range(list_length-i-1):
+
+            # go through unsorted elements (length of list - i - 1)
+            # swap an element if the next one is greater
+            if sorted_list[j] > sorted_list[j+1]:
+                # we could make a temporary variable to swap but this
+                # looks cleaner
+                sorted_list[j], sorted_list[j+1] = sorted_list[j+1], sorted_list[j]
+
+    return sorted_list
 
 
 # Uppgift 5 (att skrivas)
+
+def bandit_language(sentence):
+    vowels = ["a", "e", "i", "o", "u", "å", "ä", "ö"]
+    bandit_sentence = ""
+
+    for letter in sentence:
+
+        if not letter in vowels and letter != " ":
+            bandit_letter = letter + "o" + letter
+            bandit_sentence += bandit_letter
+        else:
+            bandit_sentence += letter
+
+    return bandit_sentence
+
+# Uppgift 6
+
+def make_bandit_dictionary(in_dictionary):
+
+    bandit_dict = {}
+
+    for key, value in in_dictionary.items():
+        bandit_value_list = []
+        for item in value:
+            bandit_value_list.append(bandit_language(item))
+        bandit_dict[key] = bandit_value_list
+
+    return bandit_dict
+
--- lab-2/using_my_module.py
+++ lab-2/using_my_module.py
@@ -1,8 +1,11 @@
+print("----------- IMPORT ---------------\n")
 
 import my_module
+import math
 
 
 # Uppgift 1 (givet)
+print("\n----------- UPG 1 ---------------\n")
 
 y = 222
 x = 111
@@ -11,19 +14,74 @@ print("Outside module: x=" + str(x) + " and x_list=" + str(x_list) + " and y=" +
 my_module.scope_testing_function(x, x_list)
 print("Outside module: x=" + str(x) + " and x_list=" + str(x_list) + " and y=" + str(y))
 
+
 # Uppgift 2 (att skrivas)
+print("\n----------- UPG 2 ---------------\n")
+
+print("x = 5:\t\t",my_module.my_function(5))
+print("x = pi/2:\t", my_module.my_function(math.pi/2))
+
 
 # Uppgift 3 (att skrivas)
+print("\n----------- UPG 3 ---------------\n")
+
+print("sum 5 dice:", my_module.roll_dice(5))
+# Minimum amount possible is 5 (if each dice rolls a 1), and
+# the maximum amount is 30 (if each dice rolls a 6)
+assert my_module.roll_dice(5) >= 5 and my_module.roll_dice(5) <= 30
+print("sum 0 dice:", my_module.roll_dice(0))
+
 
 # Uppgift 4 (att skrivas)
+print("\n----------- UPG 4 ---------------\n")
+
+list_unsorted = [5, 3, 8, 1, 4, 2, 6, 7]
+list_to_sort = list_unsorted
+list_built_in_sort = list_unsorted
+
+print("unsorted:\t\t", list_unsorted)
+
+list_built_in_sort.sort()
+print("bult-in sort:\t", list_built_in_sort)
+
+print("my sort:\t\t", my_module.my_sort_list(list_unsorted))
+
+assert list_to_sort == list_unsorted
+
 
 # Uppgift 5 (att skrivas)
+print("\n----------- UPG 5 ---------------\n")
+
+sentence = "hello my name is bandodo"
+bandit_sentence = my_module.bandit_language(sentence)
+print("sentence:\t\t", sentence)
+print("bandit version:\t", bandit_sentence)
+
+# Test sentence from the exercise description
+test_sentc = "jag talar rövarspråket"
+assert my_module.bandit_language(test_sentc) == "jojagog totalolaror rorövovarorsospoproråkoketot"
+
 
 # Uppgift 6 (givet)
+print("\n----------- UPG 6 ---------------\n")
 
 animals = {'tiger': ['claws', 'sharp teeth', 'four legs', 'stripes'],
            'elephant': ['trunk', 'four legs', 'big ears', 'gray skin'],
            'human': ['two legs', 'funny looking ears', 'a sense of humor']
            }
 
-# Uppgift 6 (att skrivas)
+def make_bandit_dictionary(in_dictionary):
+
+    bandit_dict = {}
+
+    for key, value in in_dictionary.items():
+        bandit_value_list = []
+        for item in value:
+            bandit_value_list.append(my_module.bandit_language(item))
+        bandit_dict[key] = bandit_value_list
+
+    return bandit_dict
+
+print("original dict:", animals)
+print("bandified dict:", my_module.make_bandit_dictionary(animals))
+
